55c95e127d027b5540ebe91ad91ce8bee9df2b11
sasikumari2i <sasikumar.raju@ideas2it.com>
2022-01-29 22:53:28 +0530

    ToDo29/1/2022

--- ToDo/script.js
+++ ToDo/script.js
@@ -1,8 +1,20 @@
 window.addEventListener('load', () => {
 
+    const form = document.querySelector('#new-myday-task');
+    const input = document.querySelector('#new-task-input');
+    const submit_btn = document.querySelector('#new-task-submit');
+    submit_btn.disabled = true;
+    input.addEventListener("change", () => {
+        if (document.querySelector("#new-task-input").value === "") {
+            submit_btn.disabled = true; //button remains disabled
+        } else {
+            submit_btn.disabled = false; //button is enabled
+        }
+    })
+
     const task_div = document.querySelector("#tasks");
     var mydayArr = [];
-    for (var i = 1; i <= 10; i++) {
+    for (var i = 1; i <= 8; i++) {
         const task_add = document.createElement("div");
         task_add.classList.add("center-add-myday");
         task_add.style.padding = "2px 5px 2px 5px";
@@ -10,13 +22,10 @@ window.addEventListener('load', () => {
         const myday_id = "myday" + i;
         mydayArr[i - 1] = myday_id;
         task_add.id = myday_id;
-        task_add.innerHTML += "<hr>";
         task_div.appendChild(task_add);
+        task_div.innerHTML += "<hr>";
     }
 
-    const form = document.querySelector('#new-myday-task');
-    const input = document.querySelector('#new-task-input');
-    const submit_btn = document.querySelector('#new-task-submit');
     let task_insert;
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -34,12 +43,31 @@ window.addEventListener('load', () => {
             setList.classList.add('set-btn');
         })
         input.value = "";
+        submit_btn.disabled = true;
     })
+
+
+    const add_list_input = document.querySelector('#new_list');
+    add_list_input.addEventListener('change', () => {
+        const left_list = add_list_input.value;
+        const new_ul = document.querySelector('#left_ul');
+        const new_li = document.createElement('li');
+        const new_mat_icon = document.createElement('span');
+        new_mat_icon.classList.add('material-icons');
+        new_mat_icon.innerText = "list";
+        new_ul.appendChild(new_li);
+        new_li.appendChild(new_mat_icon);
+        const new_task_span = document.createElement('span');
+        new_task_span.innerText = left_list;
+        new_li.appendChild(new_task_span);
+        //console.log(new_ul);
+    })
+
 })
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "270px";
+    document.getElementById("main").style.marginLeft = "250px";
     /* document.getElementById("add-main").style.marginLeft = "270px";*/
     /*document.getElementById("hr-line").style.marginLeft = "270px";*/
     document.getElementById("nav-btn").style.display = "none";
